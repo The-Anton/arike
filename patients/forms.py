999afb801a085b1d63fc9bff5902bb126b393eb6
The-Anton <shekhar59324@gmail.com>
2022-03-04 00:59:38 +0530

    add: patient, disease history, treatment and family custom model forms

--- patients/forms.py
+++ patients/forms.py
@@ -1,14 +1,54 @@
 from django import forms
 
-from patients.models import Patient
+from patients.models import DiseaseHistory, Family, Patient, Treatment
+
+# Patient Form
 
 class PatientCreateForm(forms.ModelForm):
     class Meta:
         model = Patient
-        fields = ["name","address", "pin_code", "phone_number", "kind"]
+        fields = ["department","first_name", "last_name", "birth_date", "email", "phone_number", "emergency_phone_number", "gender"]
 
 
-class PatientFacilityForm(forms.ModelForm):
+class PatientUpdateForm(forms.ModelForm):
     class Meta:
         model = Patient
-        fields = ["name","address", "pin_code", "phone_number", "kind"]
+        fields = ["department","first_name", "last_name", "birth_date", "email", "phone_number", "emergency_phone_number", "gender"]
+
+# Disease History Form
+class DiseaseHistoryCreateForm(forms.ModelForm):
+    class Meta:
+        model = DiseaseHistory
+        fields = ["name", "remark"]
+
+
+class DiseaseHistoryUpdateForm(forms.ModelForm):
+    class Meta:
+        model = DiseaseHistory
+        fields = ["name", "remark"]
+
+# Treatment Form
+
+class TreatmentCreateForm(forms.ModelForm):
+    class Meta:
+        model = Treatment
+        fields = ["discription"]
+
+
+class TreatmentUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Treatment
+        fields = ["discription"]
+
+# Family Form
+
+class FamilyCreateForm(forms.ModelForm):
+    class Meta:
+        model = Family
+        fields = ["first_name","last_name", "email", "birth_date", "phone_number", "address", "gender", "relation", "education"]
+
+
+class FamilyUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Family
+        fields = ["first_name","last_name", "email", "birth_date", "phone_number", "address", "gender", "relation", "education"]
