82c2338828337c78bf0d446cd40c5572c625869a
The-Anton <shekhar59324@gmail.com>
2022-03-03 17:46:52 +0530

    config: add sub app modules to setting

--- arike/settings.py
+++ arike/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
+    'facility',
+    'patients',
+    'schedule',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'arike.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
