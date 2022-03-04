8bcc373612373a3eff1fa9ee5c803ee2ca41e983
The-Anton <shekhar59324@gmail.com>
2022-03-04 21:21:00 +0530

    add: custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import ArikeUser
+
+admin.sites.site.register(ArikeUser)
--- users/models.py
+++ users/models.py
@@ -1,15 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 ROLES = (("PRIMARY NURSE", "PRIMARY NURSE"), 
         ("SECONDARY NURSE", "SECONDARY NURSE"), 
         ("ADMIN", "ADMIN"))
 
-class ArikeUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=100)
+class CustomAccountManager(BaseUserManager):
+        def create_superuser(self, email, password, **other_fields):
+                other_fields.setdefault('is_staff', True)
+                other_fields.setdefault('is_superuser', True)
+                other_fields.setdefault('is_active', True)
+
+                if other_fields.get('is_staff') is not True:
+                        raise ValueError(
+                        'Superuser must be assigned to is_staff=True.')
+                if other_fields.get('is_superuser') is not True:
+                        raise ValueError(
+                        'Superuser must be assigned to is_superuser=True.')
+
+                return self.create_user(email, password, **other_fields)
+
+        def create_user(self, email, password, **other_fields):
+                if not email:
+                        raise ValueError("You must provide an email address")
+                
+                email = self.normalize_email(email)
+                user = self.model(email=email, **other_fields)
+                user.set_password(password)
+                user.save()
+                return user
+class ArikeUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    birth_date = models.DateField(null=True)
+    department = models.CharField(max_length=100)
     role = models.CharField(max_length=100, choices=ROLES)
+    is_verified = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['first_name', 'email', 'role']
+
+    objects = CustomAccountManager()
+    
+    def __str__(self):
+        return self.email
