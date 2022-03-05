1ca1bea9babac4bc2ea18c20a48ca4522fc8e14f
The-Anton <shekhar59324@gmail.com>
2022-03-05 15:35:12 +0530

    config: production settings

--- arike/settings.py
+++ arike/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a^el(fk)%6p!&d@lw1g-2&#2(__px-647xlhz&czvg=_k1v3hf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://arike-project.herokuapp.com/"]
 
 
 # Application definition
@@ -127,3 +128,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.ArikeUser'
+
