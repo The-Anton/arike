ccec6872d2ed1bd92080ad9ac9f02d8aeb093ea5
The-Anton <shekhar59324@gmail.com>
2022-03-07 21:36:06 +0530

    config: migrate data from sqlite3 to postgresql

--- arike/settings.py
+++ arike/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'arike.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'anton',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
